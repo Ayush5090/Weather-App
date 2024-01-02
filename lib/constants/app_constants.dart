dd6b31362f436a9db2a004b09c80ca8b3bb04af5
Ayush Mishra <133570572+Ayush5090@users.noreply.github.com>
2024-01-02 10:05:00 +0530

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,4 +1,4 @@
-const String apiKey = '819e2acd9943fe7b98a8daa5b9f98904';
+const String apiKey = 'myKey';
 const String baseUrl = 'https://api.openweathermap.org/data/2.5/';
 const String currentWeatherUrl = 'weather';
 const String airPollution = 'air_pollution';
